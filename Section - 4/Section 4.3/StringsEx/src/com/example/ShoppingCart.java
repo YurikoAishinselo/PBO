56da0dab6d0b1a56858ff51ffafef1059b68ab71
YurikoAishinselo <97679660+YurikoAishinselo@users.noreply.github.com>
2022-02-18 09:30:45 +0700

    Section 4

---
+++ Section - 4/Section 4.3/StringsEx/src/com/example/ShoppingCart.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author anshenoy
+ */
+public class ShoppingCart {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        
+        int spaceIdx = custName.indexOf(" ");
+        System.out.println("Index of space character is " +spaceIdx);
+        // Get the index of the space character (" ") in custName. 
+
+        String firstName = custName.substring(0,5);
+        System.out.println("My first name is "+ firstName);
+        // Use the substring method parse out the first name and print it.
+
+
+    }
+
+    
+}
