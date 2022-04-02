ddc6ad66160b949a4522fab96744b637df579dcd
YurikoAishinselo <97679660+YurikoAishinselo@users.noreply.github.com>
2022-04-02 10:49:29 +0700

    4/2/2022

--- Section - 6/Practice/ProblemSet6_3/src/loopshape/LoopShapeTest.java
+++
@@ -1,9 +0,0 @@
-package loopshape;
-
-public class LoopShapeTest {
-    public static void main(String[] args) {
-        LoopShape.createRectangle(8, 4);
-        System.out.println("");
-        LoopShape.createTriangle(9);
-    }   
-}
